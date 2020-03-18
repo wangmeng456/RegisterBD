20c6508c1930da644ba71d1caedea1e37f091bf7
wangmeng456 <3197985967@qq.com>
2020-03-18 11:30:08 +0800

    publish

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    htmlmin:{
+      options:{
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src:'./index.html',
+        dest:'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/register.css': 'register.css'
+    },
+    uglify:{
+      'dist/register.js':'register.js'  
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default',['htmlmin','cssmin','uglify']);
+};
